66cd3263a4a1f3a9b7557129345c2203aac0f973
Schuyler <skym1n0r91@gmail.com>
2022-02-11 17:23:37 -0600

    email is connected to state

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,47 +1,38 @@
 import React from "react";
 import axios from "axios";
 
-const initialState = {
-  positionX: 1,
-  positionY: 2,
-  steps: 3,
-  email: "lady@gaga.com",
-};
+const initialState = { x: 1, y: 2, steps: 3, email: "" };
 
 const URL = "http://localhost:9000/api/result";
 
 export default class AppClass extends React.Component {
   state = initialState;
 
-  onSubmit = (event) => {
-    event.preventDefault();
-    const payLoadToSend = {
-      email: this.state.email,
-      steps: this.state.steps,
-      x: this.state.positionX,
-      y: this.state.positionY,
-    };
-    axios
-      .post(URL, payLoadToSend)
-      .then((res) => {
-        console.log(res.data.message);
-        this.setState({ ...this.state, message: res.data.message });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+  // onSubmit = (event) => {
+  //   event.preventDefault();
+  //   const payLoadToSend = {
+  //     email: this.state.email,
+  //     steps: this.state.steps,
+  //     x: this.state.x,
+  //     y: this.state.y,
+  //   };
+  //   axios
+  //     .post(URL, payLoadToSend)
+  //     .then((res) => {
+  //       this.setState({ ...this.state, message: res.data.message });
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // };
 
-  onChange = (event) => {
-    const { value, id } = event.target;
-    this.setState({ ...this.state, [id]: value });
+  emailChange = (event) => {
+    const { value } = event.target;
+    this.setState({ ...this.state, email: value });
   };
 
   render() {
-    console.log("THE STATE ---> ", this.state);
-
     const { className } = this.props;
-    console.log("PROPS", this.props);
 
     return (
       <div id="wrapper" className={className}>
@@ -72,8 +63,8 @@ export default class AppClass extends React.Component {
         </div>
         <form onSubmit={this.onSubmit}>
           <input
-            onChange={this.onChange}
-            value={this.state.email.data}
+            onChange={this.emailChange}
+            value={this.state.email}
             id="email"
             type="email"
             placeholder="type email"
